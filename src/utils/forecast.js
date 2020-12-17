8461eb6baaf58e6ffe5b27acddb66b080b83005d
avikat <anshulgupta471997@gmail.com>
2020-12-17 13:54:57 +0530

    Humidity added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ request({url,json:true},(error,{body}) => {
   }
   else{
 
-    callback(undefined,body.current.weather_descriptions[0] + ". it is currently "+body.current.temperature+" degree out. It feels like "+body.current.feelslike +' degree out' )
+    callback(undefined,body.current.weather_descriptions[0] + ". it is currently "+body.current.temperature+" degree out. It feels like "+body.current.feelslike +' degree out and the humidity is ' + body.current.humidity+' out there.' )
 }
 })
 }
